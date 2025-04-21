118452375c6513dbbc4c2c23c0b1aa139303fcd9
EbadShelby <ebadyasser123xd@gmail.com>
2025-04-22 06:11:47 +0800

    working on JS products data

---
+++ data/products.js
@@ -0,0 +1,9 @@
+const products = [
+    {
+        id: 1,
+        name: "Ebad Gwapo",
+        price: 749,
+        rating: {rate: 4.5, count: 100},
+        image: "assets/images/dress 1.jpeg"
+    }
+]
